83eb44216d09a4398c80a003cd3312f217bad034
kauassilva <kauassilva1@gmail.com>
2025-05-27 22:37:43 -0300

    Initial commit

---
+++ src/main/java/com/learning/orange_graph/OrangeGraphApplication.java
@@ -0,0 +1,13 @@
+package com.learning.orange_graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeGraphApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/orange_graph/OrangeGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.orange_graph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
