d1736f52cb76bcd8ac9e304ea740248261510109
Breno Luiz <brenofialho36@gmail.com>
2025-06-03 21:13:59 -0300

    adição da data de criação e resolução bug inversão

--- src/main/java/com/learning/orange_graph/Dto/Account_DTO.java
+++ src/main/java/com/learning/orange_graph/Dto/Account_DTO.java
@@ -1,11 +1,13 @@
 package com.learning.orange_graph.Dto;
 
+import java.time.LocalDate;
+
 import com.learning.orange_graph.Entity.Account;
 
 public record Account_DTO (String name, String cpf, String email, Double balance, Boolean suspect) {
 
     public Account toAccount() {
-        return new Account(name, cpf, email, balance, suspect);
+        return new Account(name, cpf, email, balance, suspect,  LocalDate.now());
     }
 
 }
